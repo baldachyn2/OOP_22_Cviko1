43315cacfb399fd4828881dfdf87be68146dbe00
Matúš Švantner <xsvantner@stuba.sk>
2022-03-13 11:55:26 +0100

    if

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,14 +1,12 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
-    public static void main(String[] args){
-        int podiel =2/3;
-        System.out.println(podiel);
-
-        float podielDesat = 2/3.0f;
-        System.out.println(podielDesat);
-
-        int zvysok = 2%3;
-        System.out.println(zvysok);
+    public static void main(String[] args) {
+        int cislo = 5;
+        if (cislo == 0){
+            System.out.println("Je to nula");
+        } else{
+            System.out.println("nie je to nula");
+        }
     }
 }
